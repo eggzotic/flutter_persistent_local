9033606b6ef7eec107e6b85bd227d9b2bb5e1379
Richard Shepherd <eggzotic@gmail.com>
2019-12-02 15:13:03 +0800

    align name in pubspec with repo

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:stateless_demo/main.dart';
+import 'package:flutter_create_stateless/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
